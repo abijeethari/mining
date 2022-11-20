e736ea04c84c499192a834fae668570fe812f612
abijeethari <75467195+abijeethari@users.noreply.github.com>
2022-11-20 12:33:12 +0530

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,1 +1,2 @@
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.62/lolMiner_v1.62_Lin64.tar.gz && tar -zxvf lolMiner_v1.62_Lin64.tar.gz && cd 1.62 && clear && ./lolMiner --algo ETHASH --pool asia-etc.2miners.com:1010 -u 0x4A492FdD5fea46bE16eA183999DbE80086f79D2B.teejiba
+wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz && tar -zxvf t-rex-0.26.8-linux.tar.gz && cd t-rex-0.26.8-linux && clear && ./t-rex -a etchash -o stratum+tcp://asia-etc.2miners.com:1010 -u 0x4A492FdD5fea46bE16eA183999DbE80086f79D2B -p x -w teeji1
+
