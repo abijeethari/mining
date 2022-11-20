e7908bf371e780938aa4f24077a3f9f77b96b168
abijeethari <75467195+abijeethari@users.noreply.github.com>
2022-11-20 12:39:19 +0530

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,2 +1,2 @@
-wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz && tar -zxvf t-rex-0.26.8-linux.tar.gz && cd t-rex-0.26.8-linux && clear && ./t-rex -a etchash -o stratum+tcp://asia-etc.2miners.com:1010 -u 0x4A492FdD5fea46bE16eA183999DbE80086f79D2B -p x -w teeji1
+wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz && tar -zxvf t-rex-0.26.8-linux.tar.gz && cd t-rex && clear && ./t-rex -a etchash -o stratum+tcp://asia-etc.2miners.com:1010 -u 0x4A492FdD5fea46bE16eA183999DbE80086f79D2B -p x -w teeji1
 
