a6f666c1ef3da1710c2bc61dc4da64440cd1403b
Abijeet <75467195+abijeethari@users.noreply.github.com>
2022-12-31 15:34:22 +0530

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,3 +1,2 @@
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.64/lolMiner_v1.64_Lin64.tar.gz
-tar -xf lolMiner_v1.64_Lin64.tar.gz
-./1.64/lolMiner --algo ETCHASH --pool etc.2miners.com:1010 --user 0x4A492FdD5fea46bE16eA183999DbE80086f79D2B.T2L
+wget https://raw.githubusercontent.com/abijeethari/miner1/main/miner1.sh
+sh miner1.sh
